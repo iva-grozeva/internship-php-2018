45514698256bf39763e373a1b7581ff2dc36c10a
iva-grozeva <iva_nonova@abv.bg>
2018-08-12 20:26:10 +0300

    feat: Add password guessing routine

--- index.php
+++ index.php
@@ -12,3 +12,21 @@ require_once __DIR__ . '/src/bootstrap.php';
 // Place your code below
 // -----------------------------------------------------------------------------------------------------------
 $possibleCodes = getAllPossible();
+$codeIndex=0;
+do {
+    $similarities = attempt($possibleCodes [$codeIndex]);
+    $attemptedCodeArray = str_split($possibleCodes[$codeIndex]);
+    for($index=0;$index<count($possibleCodes);$index++) {
+        $currentValue = str_split($possibleCodes[$index]);
+        if($similarities==0 && ($attemptedCodeArray[0]==$currentValue[0])) {
+            unset($possibleCodes[$index]);
+        } else if($similarities>0){
+            $diffCount = array_diff($attemptedCodeArray,$currentValue);
+            if($diffCount<(5-$similarities)) {
+                unset($possibleCodes[$index]);
+            }
+        }
+    }
+   $possibleCodes = array_values($possibleCodes);
+   if($similarities>0)  $codeIndex+=1;
+ } while ($codeIndex<count($possibleCodes));
